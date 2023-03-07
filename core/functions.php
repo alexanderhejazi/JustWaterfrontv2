2c351336f85b3c6500865da593290fdc70109d29
Alexander Hejazi <alexander@hejazi.se>
2023-03-07 01:10:40 +0100

    Remove captcha

--- core/functions.php
+++ core/functions.php
@@ -1,24 +1,5 @@
 <?php
 
-function curlRequest($URL)
-{
-
-    $request = curl_init();
-    curl_setopt($request, CURLOPT_SSL_VERIFYPEER, FALSE);
-    curl_setopt($request, CURLOPT_FOLLOWLOCATION, TRUE);
-    curl_setopt($request, CURLOPT_RETURNTRANSFER, TRUE);
-    curl_setopt($request, CURLOPT_URL, $URL);
-    curl_setopt($request, CURLOPT_TIMEOUT, 80);
-
-    $response = curl_exec($request);
-    curl_close($request);
-
-    return json_decode($response, true);
-}
-
-function googleCaptcha($userIdentifier)
-{
-    return (curlRequest($_ENV['GOOGLE_CAPTCHA_ENDPOINT'] . $userIdentifier))['success'];
-}
+// No code for now.
 
 ?>

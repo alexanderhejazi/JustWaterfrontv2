bd8c901c9d4ac2ba64f681510e6f10b6189dc571
lanceboyer1 <itslanceboyer@gmail.com>
2023-03-06 19:23:04 -0500

    Create users.sql
    users table

---
+++ users.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
